93849ca2b731d37f201bd06cbb1c44a61841c96b
galalkoro98 <galal.koro@gmail.com>
2023-02-04 14:17:35 +0100

    updated  '

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./index.css";
+import "./styles/style.css";
 import MyButtonList from "./init.js";
 import ResetBtn from "./reset.js";
 import ModeBtn from "./mode.js";
--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-// import ReactDOM from "react-dom";
-// // import { gameRules } from "./rules";
-// ReactDOM.render(<App />, document.getElementById("root"));
-// Path: src/index.js
-// Compare this snippet from src/rules.js:
-// import { createRoot } from "react-dom/server";
 
 import React from "react";
 import MyComponent from "./App.js";
